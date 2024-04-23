94bf2c3f803008f2ac2753b54dba0294539e3663
Nikita Khvorostin <khvorostinn@gmail.com>
2024-04-23 14:31:51 +0300

    upgraded storybook -

--- app/.storybook/main.ts
+++ app/.storybook/main.ts
@@ -3,11 +3,12 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@chromatic-com/storybook',
-		'@storybook/addon-interactions'
-	],
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@chromatic-com/storybook',
+        '@storybook/addon-interactions',
+        '@storybook/addon-mdx-gfm'
+    ],
 	framework: {
 		name: '@storybook/sveltekit',
 		options: {}
