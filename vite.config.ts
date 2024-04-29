0ce7c161e3ba00a138538da53497e7701690fa16
Nikita Khvorostin <khvorostinn@gmail.com>
2024-04-30 02:13:00 +0300

    test commit for vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,19 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			'uikit': "/src/stories"
-		}
+	build: {
+		lib: {
+			entry: 'src/index.ts',
+			name: 'uikit',
+			fileName: (format) => `uikit.${format}.ts`,
+		},
+		rollupOptions: {
+			external: ['svelte'],
+			output: {
+				globals: {
+					svelte: 'svelte',
+				},
+			},
+		},
 	},
 });
