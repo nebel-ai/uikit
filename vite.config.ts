2dd4e2ea22d231666bd51b428531b3aa676670ef
Nikita Khvorostin <khvorostinn@gmail.com>
2024-04-30 02:17:06 +0300

    test commit for vite v 1.2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from "path"
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'uikit': path.resolve(__dirname, 'src/stories'),
+		},
+	},
 	build: {
 		lib: {
 			entry: 'src/index.ts',
